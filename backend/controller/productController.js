cf4624b7c77ec7c208332c95ef52c61df1f93c4d
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2022-02-15 15:36:00 -0800

    initial rest api

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require("express-async-handler");
 
+const Product = require("../models/productModel");
+
 // @description Get products
 // @route GET /api/products
 // @access Private
 const getProducts = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get product" });
+  const products = await Product.find({});
+  res.status(200).json(products);
 });
 
 // @description set product
@@ -15,21 +18,43 @@ const setProduct = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  res.status(200).json({ message: "set product" });
+
+  const product = await Product.create({
+    text: req.body.text,
+  });
+  res.status(200).json(product);
 });
 
 // @description Update products
 // @route PUT /api/products/:id
 // @access Private
 const updateProduct = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update product ${req.params.id}` });
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(400);
+    throw new Error("Product not found");
+  }
+  const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedProduct);
 });
 
 // @description Delete products
 // @route Delete /api/products/:id
 // @access Private
 const deleteProduct = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete product ${req.params.id}` });
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(400);
+    throw new Error("Product not found");
+  }
+
+  await product.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
