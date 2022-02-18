453ac17c7ca2782446ad02aaf2dd773b9bb1010a
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2022-02-17 18:59:38 -0800

    dnd

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Product from "../components/Product";
+import ProductForm from "../components/ProductForm";
+import { DragDropContext } from "react-beautiful-dnd";
 
 function Products() {
+  const handleOnChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div>
-      <Product />
+      {/* <h2>Your Skincare Products</h2> */}
+      <ProductForm />
+      <Product handleOnChange={handleOnChange} />
     </div>
   );
 }
