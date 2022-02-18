292eb2f7802ae548b7ed9c7917b670d92935dc0a
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2022-02-17 22:36:53 -0800

    deployment

--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,13 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./frontend/public", "index.html"));
+});
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
